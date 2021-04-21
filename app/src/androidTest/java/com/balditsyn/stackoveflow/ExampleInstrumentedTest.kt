496e10298a14e0206c58c73b2fc08b714c353991
VladimirBalditsyn <vo.balditsyn@gmail.com>
2021-04-21 04:03:20 +0300

    added ui tests

--- app/src/androidTest/java/com/balditsyn/stackoveflow/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/balditsyn/stackoveflow/ExampleInstrumentedTest.kt
@@ -1,12 +1,22 @@
 package com.balditsyn.stackoveflow
 
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.balditsyn.stackoveflow.R
+import com.balditsyn.stackoveflow.data.repository.QuestionRepository
+import kotlinx.coroutines.selects.select
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -20,5 +30,15 @@ class ExampleInstrumentedTest {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("com.balditsyn.stackoverflow", appContext.packageName)
+    }
+
+    @Test
+    fun checkUI() {
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withId(R.id.question_list)).perform(swipeDown())
+        onView(withId(R.id.question_list)).perform(click())
+        onView(withId(R.id.answer_list)).check(matches(isDisplayed()))
+        onView(withId(R.id.answer_list)).perform(pressBack())
+        onView(withId(R.id.question_list)).check(matches(isDisplayed()))
     }
 }
